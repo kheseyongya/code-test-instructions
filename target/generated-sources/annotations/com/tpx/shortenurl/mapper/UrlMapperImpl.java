8ebe501e4f185ec7a43c84b599c21f8b9924a95e
khese.yongya <kheseyongya@gmail.com>
2025-11-26 13:02:44 +0000

    persist data using h2 file storage

--- target/generated-sources/annotations/com/tpx/shortenurl/mapper/UrlMapperImpl.java
+++ target/generated-sources/annotations/com/tpx/shortenurl/mapper/UrlMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-11-26T12:41:32+0000",
+    date = "2025-11-26T12:44:38+0000",
     comments = "version: 1.6.3, compiler: javac, environment: Java 21.0.9 (Oracle Corporation)"
 )
 @Component
